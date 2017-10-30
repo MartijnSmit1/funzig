421d3344ff05b2dc5c11e91c23d25fc1177bd804
MartijnSmit1 <martijnie4132@gmail.com>
2017-10-30 12:12:47 +0100

    Spellingsfout

--- app/header.php
+++ app/header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-	<title>Funzige Deisngs</title>
+	<title>Funzige Designs</title>
 
 	<meta name="theme-color" content="#6eb257">
 
